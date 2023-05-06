8b77d6d2b2ceafabd5fb5d7b18ab4b987571e020
delish-khadka <delishkhadka@gmail.com>
2023-05-07 00:18:18 +0545

    created dropdown using models(without ajax)

---
+++ Controllers/LocationController.cs
@@ -0,0 +1,23 @@
+﻿using DotNetCoreJquery.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreJquery.Controllers
+{
+    public class LocationController : Controller
+    {
+        public IActionResult Index()
+        {
+            CitiesInfo citiesInfo = new CitiesInfo();
+            //citiesInfo.CountryInfosList.Add(new CountryInfo { CId = 0, CName = "Select Country" });
+
+            citiesInfo.CountryInfosList.Add(new CountryInfo { CId = 101, CName = "India" });
+            citiesInfo.CountryInfosList.Add(new CountryInfo { CId = 102, CName = "Nepal" });
+            citiesInfo.CountryInfosList.Add(new CountryInfo { CId = 103, CName = "Bhutan" });
+            citiesInfo.CountryInfosList.Add(new CountryInfo { CId = 104, CName = "Bangladesh" });
+            citiesInfo.CountryInfosList.Add(new CountryInfo { CId = 105, CName = "China" });
+            citiesInfo.CountryInfosList.Add(new CountryInfo { CId = 106, CName = "Srilanka" });
+
+            return View(citiesInfo);
+        }
+    }
+}
---
+++ Models/CitiesInfo.cs
@@ -0,0 +1,52 @@
+﻿namespace DotNetCoreJquery.Models
+{
+    public class CitiesInfo
+    {
+        private List<CountryInfo> countryInfos = new();
+        public List<CountryInfo> CountryInfosList
+        { 
+            get
+            {
+                return countryInfos;
+            } 
+        set { countryInfos = value; }
+        }
+
+        private List<StateInfo> stateInfos = new();
+        public List<StateInfo> stateInfosList
+        {
+            get
+            {
+                return stateInfos;
+            }
+            set { stateInfos = value; }
+        }
+
+        /*private List<CityInfo> cityInfos = new();
+        public List<CityInfo> cityInfosList
+        {
+            get{return cityInfos;}
+            set { cityInfos = value; }
+        }*/
+        public List<CityInfo> CityInfosList { get; set; }
+    }
+    
+    public class CountryInfo
+    {
+        public int CId { get; set; }
+        public string CName { get; set; }
+    }
+    public class StateInfo
+    {
+        public int SId { get; set; }
+        public int CId { get; set; }
+        public string SName { get; set; }
+    }
+
+    public class CityInfo
+    {
+        public int CityId { get; set; }
+        public int SId { get; set; }
+        public string CityName { get; set; }
+    }
+}
